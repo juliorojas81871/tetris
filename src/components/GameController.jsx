fda241f2a004d288182fd66366c105e07b4aad76
Julio <juliorojas81871@gmail.com>
2022-08-30 10:40:09 -0400

    switch input to useEffect to fix bug if user by accident click on the screen

--- src/components/GameController.jsx
+++ src/components/GameController.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from "react"
 import '../styles/GameController.css';
 import { Action, actionForKey, actionIsDrop } from '../utilities/Input'
 import { playerController } from '../utilities/PlayerController'
@@ -6,24 +7,25 @@ import { useDropTime } from "../hooks/useDropTime";
 
 
 
-function GameController({ board, gameStats, player, setGameOver, setPlayer}) {
+function GameController({ board, gameStats, player, setGameOver, setPlayer }) {
     const [dropTime, pauseDropTime, resumeDropTime] = useDropTime({
         gameStats
     });
-    
+
     // cause the piece to slowly go down
     useInterval(() => {
         handleInput({ action: Action.SlowDrop });
     }, dropTime);
-    
+
     const onKeyUp = ({ code }) => {
+        console.log(code)
         const action = actionForKey(code);
         if (actionIsDrop(action)) resumeDropTime();
     };
-    
+
     const onKeyDown = ({ code }) => {
         const action = actionForKey(code);
-    
+
         if (action === Action.Pause) {
             if (dropTime) {
                 pauseDropTime();
@@ -31,29 +33,33 @@ function GameController({ board, gameStats, player, setGameOver, setPlayer}) {
                 resumeDropTime();
             }
         } else if (action === Action.Quit) {
-        setGameOver(true);
+            setGameOver(true);
         } else {
-        if (actionIsDrop(action)) pauseDropTime();
-        if (!dropTime) {
-            return;
-        }
-        handleInput({ action });
+            if (actionIsDrop(action)) pauseDropTime();
+            if (!dropTime) {
+                return;
+            }
+            handleInput({ action });
         }
     };
-    
-const handleInput = ({ action }) => { 
-    playerController({ action, board, player, setPlayer, setGameOver });
+
+    const handleInput = ({ action }) => {
+        playerController({ action, board, player, setPlayer, setGameOver });
     };
+
+    useEffect(() => {
+      document.addEventListener("keydown", onKeyDown)
+      document.addEventListener("keyup", onKeyUp)
     
-    return (
-        <input
-        className="GameController"
-        type="text"
-        onKeyDown={onKeyDown}
-        onKeyUp={onKeyUp}
-        autoFocus
-        />
-    );
+      return () => {
+        document.removeEventListener("keydown", onKeyDown)
+        document.removeEventListener('keyup', onKeyUp)
+      }
+    }, [onKeyDown, onKeyUp])
+    
+
+    return <div/>
+
 };
 
 export default GameController
